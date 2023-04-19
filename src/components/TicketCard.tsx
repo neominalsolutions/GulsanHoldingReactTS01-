4380b506b631f07844831de73cf055f2a92aecfd
mert.alptekin <mert.alptekin@neomina.com>
2023-04-19 14:04:48 +0300

    ticketCard

--- src/components/TicketCard.tsx
+++ src/components/TicketCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardGroup } from 'react-bootstrap';
+import { Card, CardGroup, Col, Row } from 'react-bootstrap';
 import { Ticket } from '../network/taskClient';
 
 export type TicketCardProps = {
@@ -8,26 +8,35 @@ export type TicketCardProps = {
 
 function TicketCard({ tickets }: TicketCardProps) {
 	return (
-		<CardGroup className='mt-5'>
-			{tickets.map((ticket: Ticket) => {
-				return (
-					<Card key={ticket.id}>
-						<Card.Body>
-							<Card.Title>
-								Atanan: {ticket.employeeName}
-							</Card.Title>
-							<Card.Text>{ticket.description}</Card.Text>
-						</Card.Body>
-						<Card.Footer>
-							{/* <small className='text-muted'>
+		<Row
+			xs={1}
+			md={2}
+			className='g-4 mt-5'>
+			{Array.from({ length: 2 }).map((_, idx) => (
+				<Col>
+					{tickets.map((ticket: Ticket) => {
+						return (
+							<Card
+								key={ticket.id}
+								className='mb-3'>
+								<Card.Body>
+									<Card.Title>
+										Atanan: {ticket.employeeName}
+									</Card.Title>
+									<Card.Text>{ticket.description}</Card.Text>
+								</Card.Body>
+								<Card.Footer>
+									{/* <small className='text-muted'>
 								{ticket.startDate.toString()}{' '}
 								{ticket.endDate.toString()}
 							</small> */}
-						</Card.Footer>
-					</Card>
-				);
-			})}
-		</CardGroup>
+								</Card.Footer>
+							</Card>
+						);
+					})}
+				</Col>
+			))}
+		</Row>
 	);
 }
 
